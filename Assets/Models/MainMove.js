36fecc6ed162b33ab261082e5c830dca555bbbe1
bloodyan <bloodyan@gmail.com>
2014-05-12 23:00:08 +0900

    test

---
+++ Assets/Models/MainMove.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function FindPaths(){
+	var coordX : int  = this.transform.position.x;
+	var coordY : int = this.transform.position.y;
+	var coordZ : int = this.transform.position.z;	
+}
+function Start () {
+	FindPaths();
+}
+
+function Update () {
+
+}
